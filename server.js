9a2e4a79df334a245bc00d6f1fd67ec9677ce84b
dinvho <dinvho@gmail.com>
2021-05-12 22:34:10 -0700

    new button

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ MongoClient.connect(dbConnecttionStr, {useUnifiedTopology: true})
     })
 
 app.listen( process.env.PORT || PORT, ()=> {
-    console.log('Server is running, better go catch it!')
+    console.log('Server is running!')
 })
 
